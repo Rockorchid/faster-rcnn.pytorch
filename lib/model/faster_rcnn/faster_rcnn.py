8a208090ac6a16b3fcadaade81318894f7cdc43b
Rockorchid <wangrz17@outlook.com>
2019-05-14 16:50:04 +0800

    5.14 sequeeze and excition for concat features

--- lib/model/faster_rcnn/faster_rcnn.py
+++ lib/model/faster_rcnn/faster_rcnn.py
@@ -17,11 +17,30 @@ import pdb
 import matplotlib.pyplot as plt
 from lib.model.utils.net_utils import _smooth_l1_loss, _crop_pool_layer, _affine_grid_gen, _affine_theta
 
+
+class SELayer(nn.Module):
+    def __init__(self, channel=256, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
 class _fasterRCNN(nn.Module):
     """ faster RCNN """
     def __init__(self, classes, class_agnostic):
         super(_fasterRCNN, self).__init__()
         self.classes = classes
+        self.se = SELayer()
         self.n_classes = len(classes)
         self.class_agnostic = class_agnostic
         # loss
@@ -61,6 +80,8 @@ class _fasterRCNN(nn.Module):
         base_feat1 = self.RCNN_base1(L_tensor)
         base_feat2 = self.RCNN_base2(R_tensor)
         base_feat = self.CatConv(torch.cat([base_feat1, base_feat2], 1))
+        base_feat = self.se(base_feat)
+
         # base_feat = base_feat1 - base_feat2
         # base_feat = self.RCNN_base(im_data)
 
