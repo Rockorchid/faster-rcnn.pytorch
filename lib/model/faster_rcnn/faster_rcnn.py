97925be352177333f80353c74634f001148bd99b
Rockorchid <wangrz17@outlook.com>
2019-05-15 18:15:45 +0800

    5.15 sequeeze and excition for concated features

--- lib/model/faster_rcnn/faster_rcnn.py
+++ lib/model/faster_rcnn/faster_rcnn.py
@@ -19,18 +19,19 @@ from lib.model.utils.net_utils import _smooth_l1_loss, _crop_pool_layer, _affine
 
 
 class SELayer(nn.Module):
-    def __init__(self, channel=256, reduction=16):
+    def __init__(self, reduction=16):
         super(SELayer, self).__init__()
+        self.reduction = reduction
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False),
-            nn.Sigmoid()
-        )
 
     def forward(self, x):
-        b, c, _, _ = x.size()
+        b, c, _, __ = x.size()
+        self.fc = nn.Sequential(
+            nn.Linear(c, c // self.reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(c // self.reduction, c, bias=False),
+            nn.Sigmoid()
+        ).cuda()
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
         return x * y.expand_as(x)
